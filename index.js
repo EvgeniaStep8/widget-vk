2fdadaadf097d3761b3b0588f446aec27273c00f
Evgenia Stepanova <evgeniast85es@gmail.com>
2023-09-04 15:06:19 +0300

    add local storage posts

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-const script = document.createElement("SCRIPT");
-script.src =
-  "https://api.vk.com/method/wall.get?owner_id=-9695053&domain=wildberries_shop/kartini_s_istoriei&offset=1&count=10&filter=all&access_token=2914c1c32914c1c32914c1c30d2a0143a8229142914c1c34de7a3b7b2d6996882e55c7b&v=5.131&callback=callbackFunc";
-document.getElementsByTagName("head")[0].appendChild(script);
+const posts = [];
+const count = 10;
+let countOfQueries = 0;
 
-const widget = document.querySelector(".widget");
-
-function callbackFunc(result) {
-  console.log(result);
+const callbackFunc = (result) => {
+  result.response.items.forEach((item) => {
+    posts.push(item);
+  });
   renderPosts(result.response.items);
+  setLocalStoragePosts(posts);
 }
+
+const getPosts = (offset) => {
+  const script = document.createElement("SCRIPT");
+  script.src =
+  `https://api.vk.com/method/wall.get?owner_id=-9695053&domain=wildberries_shop/kartini_s_istoriei&offset=${offset}&count=${count}&filter=all&access_token=2914c1c32914c1c32914c1c30d2a0143a8229142914c1c34de7a3b7b2d6996882e55c7b&v=5.131&callback=callbackFunc`;
+  document.getElementsByTagName("head")[0].appendChild(script);
+}
+
 const createPost = () => {
   const postTemplate = document.querySelector("#post-template").content;
   const post = postTemplate.querySelector(".post").cloneNode(true);
@@ -35,14 +43,33 @@ const addPost = ({ attachments, text, likes, reposts, views }) => {
 };
 
 const renderPosts = (data) => {
-  data.forEach((post) => addPost(post));
+  data.forEach((post) => {
+    addPost(post);
+  });
 };
 
-const checkPosition = () => {
-  const height = 500;
-  const threshold = (height / 5) * 4;
-};
+const setLocalStoragePosts = (posts) => {
+  posts = JSON.stringify(posts);
+  localStorage.setItem("posts", posts);
+}
+
+const renderWidget = () => {
+  if (localStorage.getItem("posts")) {
+    JSON.parse(localStorage.getItem("posts")).forEach((post) => {
+      posts.push(post);
+    });
+    renderPosts(posts);
+  } else {
+    getPosts(0);
+  }
+}
+
+renderWidget();
+
 
 widget.addEventListener("scroll", () => {
-  console.log(widget.scrollTop);
+  if (widget.scrollHeight - widget.scrollTop === 503) {
+    countOfQueries+=1;
+    getPosts(countOfQueries * count);
+  }
 });
