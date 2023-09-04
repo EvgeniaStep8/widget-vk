a651bd2fc380b664dd542f0bb806a366f4888d9c
Evgenia Stepanova <evgeniast85es@yandex.ru>
2023-09-04 21:30:16 +0300

    add local storage first render

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const posts = [];
+let posts = [];
 const count = 10;
-let countOfQueries = 0;
+
+const widget = document.querySelector(".widget");
 
 const callbackFunc = (result) => {
   result.response.items.forEach((item) => {
@@ -8,14 +9,13 @@ const callbackFunc = (result) => {
   });
   renderPosts(result.response.items);
   setLocalStoragePosts(posts);
-}
+};
 
 const getPosts = (offset) => {
   const script = document.createElement("SCRIPT");
-  script.src =
-  `https://api.vk.com/method/wall.get?owner_id=-9695053&domain=wildberries_shop/kartini_s_istoriei&offset=${offset}&count=${count}&filter=all&access_token=2914c1c32914c1c32914c1c30d2a0143a8229142914c1c34de7a3b7b2d6996882e55c7b&v=5.131&callback=callbackFunc`;
+  script.src = `https://api.vk.com/method/wall.get?owner_id=-164992662&domain=ddxfitness/kartini_s_istoriei&offset=${offset}&count=${count}&filter=all&access_token=2914c1c32914c1c32914c1c30d2a0143a8229142914c1c34de7a3b7b2d6996882e55c7b&v=5.131&callback=callbackFunc`;
   document.getElementsByTagName("head")[0].appendChild(script);
-}
+};
 
 const createPost = () => {
   const postTemplate = document.querySelector("#post-template").content;
@@ -30,9 +30,12 @@ const addPost = ({ attachments, text, likes, reposts, views }) => {
   post.querySelector(".text").textContent = text;
 
   if (attachments.length > 0) {
-    post.querySelector(".image").src = attachments[0].photo.sizes.find(
-      (size) => size.type === "q"
-    ).url;
+    if (attachments[0].type === "photo") {
+      post.querySelector(".image").src = attachments[0].photo.sizes.find(
+        (size) => size.type === "q"
+      ).url;
+      post.querySelector(".image").alt = attachments[0].photo.text;
+    }
   }
 
   post.querySelector(".likes").textContent = likes.count;
@@ -49,27 +52,28 @@ const renderPosts = (data) => {
 };
 
 const setLocalStoragePosts = (posts) => {
-  posts = JSON.stringify(posts);
-  localStorage.setItem("posts", posts);
-}
+  try {
+    localStorage.setItem("posts", JSON.stringify(posts));
+  } catch {
+    posts = posts.slice(count);
+    setLocalStoragePosts(posts);
+  }
+};
 
 const renderWidget = () => {
   if (localStorage.getItem("posts")) {
-    JSON.parse(localStorage.getItem("posts")).forEach((post) => {
-      posts.push(post);
-    });
+    posts = JSON.parse(localStorage.getItem("posts"));
     renderPosts(posts);
   } else {
     getPosts(0);
   }
-}
+};
 
 renderWidget();
-
+countMaxLocalStorageSize();
 
 widget.addEventListener("scroll", () => {
-  if (widget.scrollHeight - widget.scrollTop === 503) {
-    countOfQueries+=1;
-    getPosts(countOfQueries * count);
+  if (widget.scrollHeight - widget.scrollTop < 550) {
+    getPosts(posts.length);
   }
 });
